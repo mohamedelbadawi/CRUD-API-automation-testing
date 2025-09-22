1ff641946e3a2a33fa2f87783819bc85ebb03e54
mohamedelbadawi <redamohamed619@gmail.com>
2025-09-22 15:12:37 +0300

    feat/Add github actions for CI/CD

--- api/userAPI.ts
+++ api/userAPI.ts
@@ -7,24 +7,18 @@ export class UserAPI {
     constructor(private request: APIRequestContext) { }
     async getUsers() {
         return this.request.get(BASE_RESOURCE, {
-            headers: {
-                "x-api-key": "reqres-free-v1"
-            }
+           
         });
     }
     async createUser(data: { email: string; password: string }) {
         return this.request.post('register', {
             data: data,
-            headers: {
-                "x-api-key": "reqres-free-v1"
-            }
+           
         })
     }
     async getUser(id: string) {
         return this.request.get(`${BASE_RESOURCE}/${id}`, {
-            headers: {
-                "x-api-key": "reqres-free-v1"
-            }
+           
         });
     }
 }
--- playwright.config.ts
+++ playwright.config.ts
@@ -22,11 +22,15 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html',{outputFolder:'playwright-report'}], ['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://reqres.in/api/',
+    extraHTTPHeaders: {
+      'Content-Type': 'application/json',
+      "x-api-key": "reqres-free-v1"
+    },
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
